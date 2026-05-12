a878e0a7164795ecddf257771fc8d715833410c1
S-Swethaa <146078708+S-Swethaa@users.noreply.github.com>
2026-05-12 20:19:41 +0530

    Create 10.ENVIRONMENT.sv

---
+++ APB_UVM_VERI/10.ENVIRONMENT.sv
@@ -0,0 +1,22 @@
+class environment extends uvm_env;
+  `uvm_component_utils(environment)
+  
+  agent agnt;
+  scoreboard scb;
+  
+  function new(string name = "environment", uvm_component parent);
+    super.new(name,parent);
+  endfunction
+  
+  function void build_phase(uvm_phase phase);
+    super.build_phase (phase);
+    agnt = agent :: type_id :: create("agnt",this);
+    scb = scoreboard :: type_id :: create ("scb",this);
+  endfunction
+  
+  function void connect_phase(uvm_phase phase);
+    super.connect_phase(phase);
+    agnt.mon.ap.connect(scb.an_imp);
+  endfunction
+  
+endclass
